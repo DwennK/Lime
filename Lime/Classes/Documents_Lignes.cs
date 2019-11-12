40342665955410fef89e1b8f98920e09ed6acf74
Dwenn Kaufmann <Dwenn@live.fr>
2019-11-12 11:57:57 +0100

    BDD V2.7

---
+++ Lime/Classes/Documents_Lignes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib;
+using Dapper.Contrib.Extensions;
+using System.ComponentModel; //Sert à changer l'affichage du nom de la propritéé dans la BDD par un texte(Last Name au lieu de lastname par exemple)
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Lime
+{
+    //Spécifie le nom de la table à Utiliser pour Dapper Contrib (Obligatoire)
+    [Table("Documents_Lignes")]
+    public class Documents_Lignes
+    {
+        [Computed]
+        public int ID { get; set; }
+        public int ID_Documents { get; set; }
+        [Display(Name = "Code Article")]
+        public string CodeArticle { get; set; }
+        [Display(Name = "Libellé")]
+        public string Libelle { get; set; }
+        public int Ordre { get; set; }
+        [Display(Name = "Quantité")]
+        public int Quantite { get; set; }
+        [Display(Name = "Prix achat")]
+        public decimal? PrixAchatUnite { get; set; }
+        [Display(Name = "Taux TAV")]
+        public decimal? TauxTVA { get; set; }
+        [Display(Name = "Taux Remise")]
+        public decimal? TauxRemise { get; set; }
+        [Display(Name = "Prix TTC")]
+        public decimal PrixTotal { get; set; }
+        [Display(Name = "Total TVA")]
+        public string TotalTVA { get; set; }
+    }
+}
